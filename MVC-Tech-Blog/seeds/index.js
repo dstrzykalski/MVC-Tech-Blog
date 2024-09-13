589f34c0a44b19032e551ce3945545d52ace2e35
dstrzykalski <dstrzykalski@yahoo.com>
2024-09-13 17:49:04 -0400

    Reworked Seed Index

--- MVC-Tech-Blog/seeds/index.js
+++ MVC-Tech-Blog/seeds/index.js
@@ -1,28 +1,22 @@
-const seedUsers = require('./userSeeds');
-const seedPosts = require('./postSeeds');
-const seedComments = require('./commentSeeds');
+const seedComment = require('./commentSeeds');
+const seedPost = require('./postSeeds');
+const seedUser = require('./userSeeds');
 
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-  try {
-    await sequelize.sync({ force: true });
-    console.log('Database synced');
+  await sequelize.sync({ force: true });
+  console.log('\n--- Start ---\n');
+  await seedComment();
+  console.log('\n--- Comments SEEDED ---\n');
 
-    await seedUsers();
-    console.log('Users seeded');
+  await seedPost();
+  console.log('\n--- Post Seeded ---\n');
 
-    await seedPosts();
-    console.log('Posts seeded');
+  await seedUser();
+  console.log('\n--- User Seeded ---\n');
 
-    await seedComments();
-    console.log('Comments seeded');
-
-    process.exit(0);
-  } catch (err) {
-    console.error('Failed to seed database:', err);
-    process.exit(1);
-  }
+  process.exit(0);
 };
 
 seedAll();
